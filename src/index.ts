2d5964286e1a91bd7473b88686afea8dc2a867a5
Dmytro Iovenko <iovenko@gmail.com>
2024-08-29 20:53:35 -0400

    fixing errors

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 // @ts-check
 
-class Vehicle {
-  status = "stopped";
+type ValidStatus = "started" | "stopped";
 
-  constructor(make, model, wheels) {
+class Vehicle {
+  make: string;
+  model: string;
+  wheels: number;
+  status: ValidStatus = "stopped";
+
+  constructor(make: string, model: string, wheels: number) {
     this.make = make;
     this.model = model;
     this.wheels = wheels;
@@ -17,20 +22,20 @@ class Vehicle {
 }
 
 class Car extends Vehicle {
-  constructor(make, model) {
-    super(make, model, "four");
+  constructor(make: string, model: string) {
+    super(make, model, 4);
   }
 }
 
 class MotorCycle extends Vehicle {
-  constructor(make, model) {
+  constructor(make: string, model: string) {
     super(make, model, 2);
   }
 }
 
-function printStatus(vehicle) {
-  if (vehicle.status === "running") {
-    console.log("The vehicle is running.");
+function printStatus(vehicle: Vehicle) {
+  if (vehicle.status === "started") {
+    console.log("The vehicle is started.");
   } else {
     console.log("The vehicle is stopped.");
   }
@@ -39,9 +44,9 @@ function printStatus(vehicle) {
 const myHarley = new MotorCycle("Harley-Davidson", "Low Rider S");
 myHarley.start();
 printStatus(myHarley);
-console.log(myHarley.make.toUppercase());
+console.log(myHarley.make.toUpperCase());
 
 const myBuick = new Car("Buick", "Regal");
 myBuick.wheels = myBuick.wheels - 1;
 console.log(myBuick.wheels);
-console.log(myBuick.mdl);
+console.log(myBuick.model);
